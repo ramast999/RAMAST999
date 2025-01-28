7edc0633daa5850bcaba33cc7b96bbc161ebfbfb
ramast999 <69601383+ramast999@users.noreply.github.com>
2025-01-28 23:58:33 +0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,3 @@
-// Get Current Year
-function getCurrentYear() {
-    var d = new Date();
-    var year = d.getFullYear();
-    document.querySelector("#displayDateYear").innerText = year;
-}
-getCurrentYear()
-
 //client section owl carousel
 $(".owl-carousel").owlCarousel({
     loop: true,
@@ -31,12 +23,3 @@ $(".owl-carousel").owlCarousel({
     }
 });
 
-/** google_map js **/
-
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
